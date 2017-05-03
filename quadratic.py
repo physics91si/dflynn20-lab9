badcd4c1a1af0c41af4a73ce6eb9913d8d06da78
Donald Flynn <donaldflynn@rescomp-16-313554.stanford.edu>
2017-05-02 19:19:42 -0700

    I have completed the lab

--- quadratic.py
+++ quadratic.py
@@ -9,16 +9,31 @@ def main():
     a = sys.argv[1]
     b = sys.argv[2]
     c = sys.argv[3]
-    x1, x2 = find_roots(a, b, c)
-    print ("This is x1: %f" %x1)
-    print ("This is x2: %f" %x2)
+    num1 = 0
+    num2 = 0
+    num3 = 0
+    while True:
+        try:
+            num1 = float(a)
+            num2 = float(b)
+            num3 = float(c)
+            assert num1 != 0
+            break
+        except:    
+            print("Please input only integers")
+            a = input('Enter leading nonzero coefficient: ')
+            b = input('Enter second coefficient: ')
+            c = input('Enter constant: ')
+    x1, x2 = find_roots(int(num1), int(num2), int(num3))
+    print ("This is x1: ", x1)
+    print ("This is x2: ", x2)
 
 
 def find_roots(a,b,c):
-    mid = b^2 - 4*a*c
-    sqrt_mid = mid^(1/2)
-    x1 = (-b + sqrt_mid)/2*a
-    x2 = (-b - sqrt_mid)/2*a
+    mid = b**2 - 4*a*c
+    sqrt_mid = mid**(1/2)
+    x1 = (-b + sqrt_mid)/(2*a)
+    x2 = (-b - sqrt_mid)/(2*a)
     return x1, x2
 
 
